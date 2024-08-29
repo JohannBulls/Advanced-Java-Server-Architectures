1ed57c8d27e766e88bdd52931051dfa639b1cecc
JohannBulls <amayapez99@gmail.com>
2024-08-29 15:58:54 -0500

    HelloServices

--- src/main/java/co/edu/escuelaing/Reflexion.java
+++
@@ -1,7 +0,0 @@
-package co.edu.escuelaing;
-
-public class Reflexion {
-    public static void main(String[] args) {
-        System.out.println("Hello World! " + args[0] + " " + args[1]);
-    }
-}
